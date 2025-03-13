63f8e2e0ea7256e88502b19cc669fc3aeedd7c51
hamidkrml <karimlihamid4@gmail.com>
2025-03-13 21:11:57 +0300

    Initial Commit

---
+++ UIkitProgramaticOne/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIkitProgramaticOne
+//
+//  Created by hamid on 13.03.25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
